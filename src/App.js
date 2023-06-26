c6a0a048d8632e0d21efc7ada000dc70472e55fc
LSzpilowski <lszpilowski@gmail.com>
2023-06-26 19:03:29 +0200

    Creating and adding functionality to Featured Reactive Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import TopNav from "./components/TopNav";
+import Featured from "./components/Featured";
 
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <TopNav />
+      <Featured />
     </div>
   );
 }
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -22,7 +22,7 @@ const TopNav = () => {
           Yum
           <span className="font-bold">Eats</span>
         </h1>
-        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 ml-2 text-[14px]">
           <p className="bg-orange-700 text-white rounded-full p-2 text-bold">
             Free
           </p>
@@ -34,10 +34,10 @@ const TopNav = () => {
         <input
           className="bg-transparent p-2 w-full focus:outline-none"
           type="text"
-          placeholder="search meals"
+          placeholder="Search meals"
         />
       </div>
-      <button className="bg-orange-700 text-white hidden md:flex items-center  py-2 rounded-full">
+      <button className="bg-orange-700 text-white hidden md:flex items-center mr-6 py-2 rounded-full">
         <BsFillCartFill size={20} className="mr-2"/> Cart
       </button>
       {sideNav ? (
@@ -55,32 +55,32 @@ const TopNav = () => {
       >
         <AiOutlineClose onClick={() => setSideNav(!sideNav)} size={25}
         className="absolute right-4 top-4 cursor-pointer"/>
-        <h2 className="text-4xl p-4">Yum <span className="text-orange-700 font-bold">Eats</span></h2>
+        <h2 className="text-4xl p-4">Yum<span className="text-orange-700 font-bold">Eats</span></h2>
       <nav>
         <ul className="flex flex-col p-4 text-gray-900">
           <li className="text-xl py-4 flex">
             <BsPerson size={35}
-            className="mr-5 text-white bg-black rounded-full p-1"
+            className="mr-5 text-white bg-orange-700 rounded-full p-1"
             /> My Account
           </li>
           <li className="text-xl py-4 flex">
             <TbTruckReturn size={35}
-            className="mr-5 text-white bg-black rounded-full p-1"
+            className="mr-5 text-white  bg-orange-700 rounded-full p-1"
             /> Delivery
           </li>
           <li className="text-xl py-4 flex">
             <MdOutlineFavorite size={35}
-            className="mr-5 text-white bg-black rounded-full p-1"
+            className="mr-5 text-white  bg-orange-700 rounded-full p-1"
             /> My Favourite
           </li>
           <li className="text-xl py-4 flex">
             <FaGoogleWallet size={35}
-            className="mr-5 text-white bg-black rounded-full p-1"
+            className="mr-5 text-white  bg-orange-700 rounded-full p-1"
             /> My Wallet
           </li>
           <li className="text-xl py-4 flex">
             <MdHelp size={35}
-            className="mr-5 text-white bg-black rounded-full p-1"
+            className="mr-5 text-white  bg-orange-700 rounded-full p-1"
             /> Help
           </li>
         </ul>
