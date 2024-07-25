26af7bbf40045420b7359898494ce801095e8b2e
LSzpilowski <lszpilowski@gmail.com>
2024-07-25 10:50:18 +0100

    fix/display data on /home

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,166 +1,211 @@
-
-
-interface ITopPicks {
-  id: number;
-  title: string;
-  price: string;
-  img: string;
-}
-
 interface IMealData {
   id: number;
   name: string;
   category: string;
   image: string;
   price: string;
+  isTopPick: boolean;
 }
 
-export const topPicks: ITopPicks[] = [
+export const mealData: IMealData[] = [
   {
     id: 1,
-    title: 'Shwarma',
-    price:'8,50$',
-    img: '/img/shwarma.png'
+    name: 'Bruschetta al Pomodoro',
+    category: 'starters',
+    image: '/bruschetta_al_pomodoro.png',
+    price: '£5.00',
+    isTopPick: true
   },
   {
     id: 2,
-    title: 'Fruite Juice',
-    price:'3,50$',
-    img: '/img/fruite-juice.png'
+    name: 'Caprese Salad',
+    category: 'starters',
+    image: '/caprese_salad.png',
+    price: '£6.50',
+    isTopPick: false
   },
   {
     id: 3,
-    title: 'Yogurt Parfait',
-    price:'8,50$',
-    img: '/img/yogurt-parfait.png'
+    name: 'Antipasto Platter',
+    category: 'starters',
+    image: '/antipasto_platter.png',
+    price: '£8.00',
+    isTopPick: false
   },
   {
     id: 4,
-    title: 'Eggs with Plantain',
-    price:'11$',
-    img: '/img/egg-plantain.png'
+    name: 'Calamari Fritti',
+    category: 'starters',
+    image: '/calmari_fritti.png',
+    price: '£7.50',
+    isTopPick: true
   },
   {
     id: 5,
-    title: 'Tilapia Fish',
-    price:'16$',
-    img: '/img/tilapia-fish.png'
+    name: 'Garlic Bread',
+    category: 'starters',
+    image: '/garlic_bread.png',
+    price: '£4.00',
+    isTopPick: true
   },
   {
     id: 6,
-    title: 'Jollof Rice',
-    price:'3$',
-    img: '/img/jollof-rice.png'
+    name: 'Margherita Pizza',
+    category: 'main courses',
+    image: '/margherite_pizza.png',
+    price: '£9.00',
+    isTopPick: true
   },
   {
     id: 7,
-    title: 'Chicken Burger',
-    price:'12$',
-    img: '/img/chicken-burger.png'
+    name: 'Spaghetti Carbonara',
+    category: 'main courses',
+    image: '/spaghetti_carbonara.png',
+    price: '£10.50',
+    isTopPick: true
   },
   {
     id: 8,
-    title: 'African Salad',
-    price:'9$',
-    img: '/img/african-salad.png'
+    name: 'Lasagna al Forno',
+    category: 'main courses',
+    image: '/lasagna_al_forno.png',
+    price: '£11.00',
+    isTopPick: true
   },
   {
     id: 9,
-    title: 'Chinese Pizza',
-    price:'8,50$',
-    img: '/img/chineses-pizza.png'
+    name: 'Chicken Parmigiana',
+    category: 'main courses',
+    image: '/chicken_parmigiana.png',
+    price: '£12.50',
+    isTopPick: false
   },
   {
     id: 10,
-    title: 'Pierogi',
-    price:'200$',
-    img: '/img/pierogi.png'
+    name: 'Risotto ai Funghi',
+    category: 'main courses',
+    image: '/risotto_ai_funghi.png',
+    price: '£11.00',
+    isTopPick: false
+  },
+  {
+    id: 11,
+    name: 'Tiramisu',
+    category: 'desserts',
+    image: '/tiramisu.png',
+    price: '£6.00',
+    isTopPick: true
+  },
+  {
+    id: 12,
+    name: 'Panna Cotta',
+    category: 'desserts',
+    image: '/panna_cotta.png',
+    price: '£5.50',
+    isTopPick: false
+  },
+  {
+    id: 13,
+    name: 'Cannoli',
+    category: 'desserts',
+    image: '/cannoli.png',
+    price: '£5.00',
+    isTopPick: true
+  },
+  {
+    id: 14,
+    name: 'Gelato',
+    category: 'desserts',
+    image: '/gelato.png',
+    price: '£4.00',
+    isTopPick: false
+  },
+  {
+    id: 15,
+    name: 'Affogato',
+    category: 'desserts',
+    image: '/affogato.png',
+    price: '£5.50',
+    isTopPick: true
+  },
+  {
+    id: 16,
+    name: 'Espresso',
+    category: 'hot drinks',
+    image: '/espresso.png',
+    price: '£2.50',
+    isTopPick: true
+  },
+  {
+    id: 17,
+    name: 'Cappuccino',
+    category: 'hot drinks',
+    image: '/cappuccino.png',
+    price: '£3.00',
+    isTopPick: false
+  },
+  {
+    id: 18,
+    name: 'Latte',
+    category: 'hot drinks',
+    image: '/latte.png',
+    price: '£3.00',
+    isTopPick: false
+  },
+  {
+    id: 19,
+    name: 'Americano',
+    category: 'hot drinks',
+    image: '/americano.png',
+    price: '£2.50',
+    isTopPick: false
+  },
+  {
+    id: 20,
+    name: 'Tea',
+    category: 'hot drinks',
+    image: '/tea.png',
+    price: '£2.00',
+    isTopPick: false
+  },
+  {
+    id: 21,
+    name: 'Italian Sodas',
+    category: 'soft drinks',
+    image: '/italian_sodas.png',
+    price: '£3.50',
+    isTopPick: false
+  },
+  {
+    id: 22,
+    name: 'Limonata',
+    category: 'soft drinks',
+    image: '/limonata.png',
+    price: '£3.00',
+    isTopPick: false
+  },
+  {
+    id: 23,
+    name: 'Aranciata',
+    category: 'soft drinks',
+    image: '/arnaciata.png',
+    price: '£3.00',
+    isTopPick: false
+  },
+  {
+    id: 24,
+    name: 'Mineral Water',
+    category: 'soft drinks',
+    image: '/mineral_water.png',
+    price: '£2.00',
+    isTopPick: false
+  },
+  {
+    id: 25,
+    name: 'Cola',
+    category: 'soft drinks',
+    image: '/cola.png',
+    price: '£2.50',
+    isTopPick: false
   }
-]
-
-export const mealData: IMealData[] = [ 
-  {
-    id:1,
-    name: 'Margerita',
-    category: 'pizza',
-    image: '/img/pizza-margherita.png',
-    price: '19,9$'
-  },
-  {
-    id:2,
-    name: 'Chinese',
-    category: 'pizza',
-    image: '/img/chineses-pizza.png',
-    price: '19,9$'
-  },
-  {
-    id:3,
-    name: 'Hawaiian',
-    category: 'pizza',
-    image: '/img/pizza-hawaiian.png',
-    price: '19,9$'
-  },
-  {
-    id:4,
-    name: 'Four Cheese',
-    category: 'pizza',
-    image: '/img/pizza-four-cheese.png',
-    price: '19,9$'
-  },
-  {
-    id:5,
-    name: 'African Salad',
-    category: 'salad',
-    image: '/img/african-salad.png',
-    price: '12,9$'
-  },
-  {
-    id:6,
-    name: 'Beef Salad',
-    category: 'salad',
-    image: '/img/beef-salad.png',
-    price: '12,9$'
-  },
-{
-    id:7,
-    name: 'Mango Salad',
-    category: 'salad',
-    image: '/img/mango-salad.png',
-    price: '12,9$'
-  },
-  {
-    id:8,
-    name: 'Pomelo & Prawn Salad',
-    category: 'salad',
-    image: '/img/pomelo-prawn-salad.png',
-    price: '12,9$'
-  },
-  {
-    id:9,
-    name: 'Four cheese garlic bread',
-    category: 'appetizer',
-    image: '/img/four-cheese-garlic-bread.png',
-    price: '9,99$'
-  },
-  {
-    id:10,
-    name: 'Baked Sweet Potato Fries',
-    category: 'appetizer',
-    image: '/img/baked-sweet-potato-fries.png',
-    price: '9,99$'
-  },
-  {
-    id:11,
-    name: 'Fried calmari',
-    category: 'appetizer',
-    image: '/img/fried-calmari.png',
-    price: '9,99$'
-  },
-  {
-    id:12,
-    name: 'Stuffed mushrooms',
-    category: 'appetizer',
-    image: '/img/stuffed-mushrooms.png',
-    price: '9,99$'
-  }
-]
+];
